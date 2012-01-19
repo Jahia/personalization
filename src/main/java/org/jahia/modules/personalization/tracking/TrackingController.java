f6cc19b2f6aeb0a1b28326de2e88bff2adf88cf0
shuber <shuber@9a65c2d3-6ba1-4558-8266-50ab4f45a8ae>
2012-01-19 18:33:14 +0000

    Add new servlet to retrieve and update tracking data using REST-like operations. Also added a new tracking data updater that uses AJAX post to update the tracking data.

---
+++ src/main/java/org/jahia/modules/personalization/tracking/TrackingController.java
@@ -0,0 +1,65 @@
+package org.jahia.modules.personalization.tracking;
+
+import org.jahia.bin.JahiaController;
+import org.json.JSONException;
+import org.json.JSONObject;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.util.List;
+import java.util.Map;
+
+/**
+ * Spring servlet controller to expose and modify tracking data.
+ */
+public class TrackingController extends JahiaController {
+
+    private String trackingSessionName = "org.jahia.modules.personalization.trackingData";
+
+    public void setTrackingSessionName(String trackingSessionName) {
+        this.trackingSessionName = trackingSessionName;
+    }
+
+    public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+
+        if (request.getMethod().equalsIgnoreCase("get")) {
+            sendTrackingData(request, response);
+        } else if (request.getMethod().equalsIgnoreCase("post")) {
+            updateTrackingData(request, response);
+        }
+        return null;  //To change body of implemented methods use File | Settings | File Templates.
+    }
+
+
+    private void sendTrackingData(HttpServletRequest request, HttpServletResponse response) throws JSONException, IOException {
+
+        TrackingData trackingData = (TrackingData) request.getSession().getAttribute(trackingSessionName);
+
+        JSONObject jsonObject = new JSONObject();
+        if (trackingData == null) {
+            jsonObject.write(response.getWriter());
+            return;
+        }
+
+        for (Map.Entry<String,List<String>> trackingEntry : trackingData.getTrackingMap().entrySet()) {
+            jsonObject.put(trackingEntry.getKey(), trackingEntry.getValue());
+        }
+
+        jsonObject.write(response.getWriter());
+
+    }
+
+    private void updateTrackingData(HttpServletRequest request, HttpServletResponse response) {
+        TrackingData trackingData = (TrackingData) request.getSession().getAttribute(trackingSessionName);
+
+        Map<String,String[]> parameterMap = (Map<String,String[]>) request.getParameterMap();
+        for (Map.Entry<String,String[]> parameterMapEntry : parameterMap.entrySet()) {
+            for (String parameterValue : parameterMapEntry.getValue()) {
+                trackingData.addStringToSet(parameterMapEntry.getKey(), parameterValue);
+            }
+        }
+    }
+
+}
--- src/main/webapp/jnt_trackingDataViewer/html/trackingDataViewer.jsp
+++ src/main/webapp/jnt_trackingDataViewer/html/trackingDataViewer.jsp
@@ -6,9 +6,11 @@
 <%@ taglib prefix="fn" uri="http://java.sun.com/jsp/jstl/functions" %>
 <template:addResources type="css" resources="personalization.css"/>
 
-<c:set value="${org.jahia.modules.personalization.trackingData}" var="trackingData" />
+<c:set value="${sessionScope['org.jahia.modules.personalization.trackingData']}" var="trackingData" />
 <c:set value="${trackingData.trackingMap}" var="trackingMap" />
 <c:set value="${trackingMap.urls}" var="urls" />
+<c:set value="${trackingMap.locales}" var="locales" />
+<c:set value="${trackingMap.locales}" var="hosts" />
 
 <c:choose>
     <c:when test="${renderContext.editMode}">
@@ -22,6 +24,8 @@
                 </tr>
                 <tr>
                     <td>URLs</td><td><c:out value="${urls}"/></td>
+                    <td>Locale</td><td><c:out value="${locales}"/></td>
+                    <td>Hosts</td><td><c:out value="${hosts}"/></td>
                 </tr>
             </table>
         </div>
