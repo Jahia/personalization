b13a38c135d0b0e6909b0981566f93fa7d6454cb
shuber <shuber@9a65c2d3-6ba1-4558-8266-50ab4f45a8ae>
2012-02-07 07:51:48 +0000

    Fix NPE when using JMeter

--- src/main/java/org/jahia/modules/personalization/tracking/trackers/BrowserTracker.java
+++ src/main/java/org/jahia/modules/personalization/tracking/trackers/BrowserTracker.java
@@ -13,8 +13,15 @@ public class BrowserTracker implements TrackerInterface {
     public boolean track(HttpServletRequest request, HttpServletResponse response, TrackingData trackingData) {
         if (request.getHeader("User-Agent") != null) {
             UserAgent userAgent = UserAgent.parseUserAgentString(request.getHeader("User-Agent"));
-            trackingData.addValue("browser", userAgent.getBrowser().getName());
-            trackingData.addValue("browser-version", userAgent.getBrowserVersion().getVersion());
+            if (userAgent == null) {
+                return true;
+            }
+            if (userAgent.getBrowser() != null) {
+                trackingData.addValue("browser", userAgent.getBrowser().getName());
+            }
+            if (userAgent.getBrowserVersion() != null) {
+                trackingData.addValue("browser-version", userAgent.getBrowserVersion().getVersion());
+            }
         }
         return true;
     }
